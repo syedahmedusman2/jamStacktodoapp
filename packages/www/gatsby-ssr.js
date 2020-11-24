3a8ae76c7dc15aaf791407d3789b317b1bb39d7f
ahmed <ahmedusmanpakistan@gmail.com>
2020-11-24 15:32:56 +0500

    commit after installing netlify

---
+++ packages/www/gatsby-ssr.js
@@ -0,0 +1,20 @@
+const React = require("react");
+const wrapRootElement = require("./wrap-root-element");
+
+exports.wrapRootElement = wrapRootElement;
+
+// const React = require("react")
+// const wrapRootElement = require("./wrap-root-element");
+
+// // const { Provider } = require("react-redux")
+
+// // const createStore = require("./src/state/createStore")
+// // const store = createStore()
+
+// exports.wrapRootElement = wrapRootElement;
+// //   return (
+// //     <Provider store={store}>
+// //       {element}
+// //     </Provider>
+// //   )
+// // }
--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import {Container, Heading, Button, Flex} from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
 
-export default props => (
-    <div>
-        Hello World
-    </div>
-);
+export default props => {
+    useEffect(()=>{
+        netlifyIdentity.init({})
+    });
+    
+    return (
+    <Container>
+        <Flex sx={{flexDirection: " column", padding: 3}}>
+        <Heading as="h1">Todo App</Heading>
+        <Button sx={{margin:2, color: 'black'}}
+        onClick= { () => {alert("Login")} }
+        >Login</Button>
+        </Flex>
+    </Container>
+    
+);}
