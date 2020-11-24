fcf1cebd6a22c147762a0fa049b53f3c5a8c7b4d
ahmed <ahmedusmanpakistan@gmail.com>
2020-11-24 16:43:01 +0500

    Update index.js

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,26 +1,43 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import {Container, Heading, Button, Flex, NavLink } from "theme-ui";
 import netlifyIdentity from "netlify-identity-widget";
+import {Link} from 'gatsby';
 
 export default props => {
+    const [user, setUser]=  useState();
+
     useEffect(()=>{
         netlifyIdentity.init({})
+        
     });
+
+    netlifyIdentity.on("login", user=>{
+        netlifyIdentity.close();
+        setUser(user);
+    });
+
+    netlifyIdentity.on("logout", () =>{
+        netlifyIdentity.close();
+    })
     
     return (
+        
+    <Container>
         <Flex as='nav'>
-  <NavLink href='#!' p={2}>
+  <NavLink as={Link} to="/" p={2}>
     Home
   </NavLink>
-  <NavLink href='#!' p={2}>
-    Blog
+  <NavLink as={Link} to="/app" p={2}>
+    Dashboard
   </NavLink>
-  <NavLink href='#!' p={2}>
+  {user &&(<NavLink p={2}>
+      {user.user_metdata.full_name}
+  </NavLink>)}
+  {/* <NavLink href='#!' p={2}>
     About
-  </NavLink>
+  </NavLink> */}
 </Flex>
 
-    <Container>
         <Flex sx={{flexDirection: " column", padding: 3}}>
         <Heading as="h1">Todo App</Heading>
         <Button sx={{margin:2, color: 'black'}}
