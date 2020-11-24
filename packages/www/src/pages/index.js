ccbc445978f7b30e34e8d6543690b960748878ec
ahmed <ahmedusmanpakistan@gmail.com>
2020-11-24 16:22:33 +0500

    Update index.js

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,5 +1,5 @@
 import React,{useEffect} from 'react';
-import {Container, Heading, Button, Flex} from "theme-ui";
+import {Container, Heading, Button, Flex, NavLink } from "theme-ui";
 import netlifyIdentity from "netlify-identity-widget";
 
 export default props => {
@@ -8,11 +8,23 @@ export default props => {
     });
     
     return (
+        <Flex as='nav'>
+  <NavLink href='#!' p={2}>
+    Home
+  </NavLink>
+  <NavLink href='#!' p={2}>
+    Blog
+  </NavLink>
+  <NavLink href='#!' p={2}>
+    About
+  </NavLink>
+</Flex>
+
     <Container>
         <Flex sx={{flexDirection: " column", padding: 3}}>
         <Heading as="h1">Todo App</Heading>
         <Button sx={{margin:2, color: 'black'}}
-        onClick= { () => {alert("Login")} }
+        onClick= { () => {netlifyIdentity.open()} }
         >Login</Button>
         </Flex>
     </Container>
