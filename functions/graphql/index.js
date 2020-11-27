0bf3bbf514b25d2b6f5003af4c6b2bf11124e7e8
ahmed <ahmedusmanpakistan@gmail.com>
2020-11-27 20:33:31 +0500

    JamStack

---
+++ functions/graphql/index.js
@@ -0,0 +1,18 @@
+const {ApolloServer, gql} = require('apollo-server-lambda');
+
+const typeDefs = gql`
+type Query{
+    hello: String
+} `;
+
+const resolvers = {
+    Query:{
+        hello: () => 'Hello-World!',
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true
+})
--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -1,4 +1,11 @@
+// import React, {useContext} from 'react';
+// import {Container, Flex, Heading, Button, NavLink} from 'theme-ui'
+
+
+
+
 import React from "react";
+import {inputRef, Input, Container, Heading, Button, Flex, Label, NavLink } from "theme-ui";
 // import { render } from "react-dom"
 import { Router, Link } from "@reach/router";
 import netlifyIdentity from "netlify-identity-widget";
@@ -14,10 +21,47 @@ return (<div>{user && user.user_metadata.full_name}</div>)
 
 export default props => {
     return (
+      <div>
         <Router>
     {/* <Home path="/" /> */}
     <Dash path="/app" />
+
+    
   </Router>
+  <h1>Eh</h1>
+
+  <Flex as="form" onSubmit={e=>{e.preventDefault();
+  alert(inputRef.current.value);
+  }}>
+    <Label sx={{display: "flex"}}>
+  <span>Add&nbsp;Todo</span>
+
+  <Input ref={inputRef}
+  defaultValue='Hello'
+/>
+
+</Label>
+
+<Button variant='secondary'>Boop</Button>
+  </Flex>
+
+
+
+        {/* <Flex sx={{flexDirection: " column", padding: 3}}>
+        <Heading as="h1">Todo App</Heading>
+        <Button sx={{margin:2, color: 'black'}}
+        onClick= { () => {netlifyIdentity.open()} }
+        >Login</Button>
+        </Flex> */}
+
+        
+
+
+
+        {/* <Button mr={2}>Beep</Button> */}
+
+
+  </div>
         
     );
 };
